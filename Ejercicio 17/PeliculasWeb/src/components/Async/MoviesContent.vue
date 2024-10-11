61eb2e66df45f9aa16e4214fec50cd538591340e
Phanesan <48665230+Phanesan@users.noreply.github.com>
2024-10-10 19:00:20 -0700

    avance

---
+++ Ejercicio 17/PeliculasWeb/src/components/Async/MoviesContent.vue
@@ -0,0 +1,112 @@
+<template>
+    <div class="cards" v-for="movie in movies">
+        <div class="card">
+            <div class="imgLabel">
+                <img src="" alt="Avatar">
+            </div>
+            <div class="contentCard">
+                <h2>Title</h2>
+                <p>Description</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MoviesContent',
+    props: {
+        movies: {
+            type: Array,
+            required: true
+        }
+    },
+    setup(movies) {
+        console.log('movies', movies)
+    },
+    methods: {
+        
+    },
+    mounted() {
+        
+    }
+};
+</script>
+
+<style scoped>
+.cards {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    height: 68vh;
+    width: 28vw;
+}
+
+.card {
+    display: flex;
+    justify-content: top;
+    align-items: center;
+    flex-direction: column;
+    height: 28rem;
+    width: 18rem;
+    background-color: #3d3d3d;
+    margin-left: 2vw;
+    margin-right: 2vw;
+    margin-top: 2vh;
+    margin-bottom: 2vh;
+    border-radius: 0.6rem;
+}
+
+.imgLabel {
+    display: flex;
+    justify-content: center;
+    align-items: start;
+    width: 100%;
+    height: 68%;
+    border-top-left-radius: 0.6rem;
+    border-top-right-radius: 0.6rem;
+    img {
+        max-width: 100%;
+        max-height: 100%;
+        border-top-left-radius: 0.6rem;
+        border-top-right-radius: 0.6rem;
+        object-fit: cover;
+    }
+}
+
+.contentCard {
+    display: flex;
+    justify-content: start;
+    align-items: start;
+    flex-direction: column;
+    height: 32%;
+    width: 100%;
+    border-bottom-left-radius: 0.6rem;
+    border-bottom-right-radius: 0.6rem;
+    padding-left: 1rem;
+    padding-right: 1rem;
+    max-width: 100%;
+    
+    h2 {
+        color: var(--color-text);
+        margin-top: 0.15rem;
+        max-width: 100%;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+    }
+    p {
+        color: var(--color-text);
+        margin-top: 0.6rem;
+        max-width: 100%;
+        display: -webkit-box;
+        line-clamp: 3;
+        -webkit-line-clamp: 3;
+        -webkit-box-orient: vertical;
+        word-wrap: break-word;
+        overflow: hidden;
+        text-overflow: ellipsis;
+    }
+}
+</style>
--- Ejercicio 17/PeliculasWeb/src/components/MainPage.vue
+++ Ejercicio 17/PeliculasWeb/src/components/MainPage.vue
@@ -2,17 +2,7 @@
     <div class="content">
         <Suspense>
             <template #default>
-                <div class="cards">
-                    <div class="card">
-                        <div class="imgLabel">
-                            <img src="/test.jpg" alt="Avatar">
-                        </div>
-                        <div class="contentCard">
-                            <h2>Title</h2>
-                            <p>Description</p>
-                        </div>
-                    </div>
-                </div>
+                <MoviesContent :movies="moviesData" />
             </template>
 
             <template #fallback>
@@ -23,9 +13,44 @@
 </template>
 
 <script>
+import axios from "axios";
+import { ref } from "vue";
+import { defineAsyncComponent } from "vue";
+import MoviesContent from "./Async/MoviesContent.vue";
+
 export default {
     name: "MainPage",
+    data() {
+        const moviesData = ref([]);
+
+        return {
+            moviesData
+        }
+    },
+    mounted() {
+        const api_key = "b268f05dee97359b50ef5b57232a727a";
+
+        let config = {
+        method: 'get',
+        maxBodyLength: Infinity,
+        url: `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&page=1&language=es-MX&api_key=${api_key}`,
+        headers: { }
+        };
+
+        axios.request(config)
+        .then((response) => {
+            this.moviesData = response.data.results;
+            console.log(this.moviesData);
+        })
+        .catch((error) => {
+        console.log(error);
+        });
+    },
+    components: {
+        MoviesContent: defineAsyncComponent(() => import('./Async/MoviesContent.vue'))
+    }
 };
+
 </script>
 
 <style scoped>
@@ -33,83 +58,9 @@ export default {
     display: flex;
     justify-content: center;
     align-items: center;
+    flex-direction: column;
+    flex: wrap;
     height: 100%;
     width: 80vw;
-}
-
-.cards {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-wrap: wrap;
-    height: 100%;
-    width: 100%;
-}
-
-.card {
-    display: flex;
-    justify-content: top;
-    align-items: center;
-    flex-direction: column;
-    height: 28rem;
-    width: 18rem;
-    background-color: #3d3d3d;
-    margin-left: 2vw;
-    margin-right: 2vw;
-    margin-top: 2vh;
-    margin-bottom: 2vh;
-    border-radius: 0.6rem;
-}
-
-.imgLabel {
-    display: flex;
-    justify-content: center;
-    align-items: start;
-    width: 100%;
-    height: 68%;
-    border-top-left-radius: 0.6rem;
-    border-top-right-radius: 0.6rem;
-    img {
-        max-width: 100%;
-        max-height: 100%;
-        border-top-left-radius: 0.6rem;
-        border-top-right-radius: 0.6rem;
-        object-fit: cover;
-    }
-}
-
-.contentCard {
-    display: flex;
-    justify-content: start;
-    align-items: start;
-    flex-direction: column;
-    height: 32%;
-    width: 100%;
-    border-bottom-left-radius: 0.6rem;
-    border-bottom-right-radius: 0.6rem;
-    padding-left: 1rem;
-    padding-right: 1rem;
-    max-width: 100%;
-    
-    h2 {
-        color: var(--color-text);
-        margin-top: 0.15rem;
-        max-width: 100%;
-        white-space: nowrap;
-        overflow: hidden;
-        text-overflow: ellipsis;
-    }
-    p {
-        color: var(--color-text);
-        margin-top: 0.6rem;
-        max-width: 100%;
-        display: -webkit-box;
-        line-clamp: 3;
-        -webkit-line-clamp: 3;
-        -webkit-box-orient: vertical;
-        word-wrap: break-word;
-        overflow: hidden;
-        text-overflow: ellipsis;
-    }
 }
 </style>
